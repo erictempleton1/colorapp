1a2254356fe0886d010c46c67a4c4eca90751c88
erictempleton1 <erictempleton1@gmail.com>
2017-11-28 20:32:43 -0500

    working on while loop and queries

--- example_loader.py
+++ example_loader.py
@@ -1,20 +1,26 @@
 import sqlite3, time
 
 conn = sqlite3.connect("color_app.db")
-c = conn.cursor()
 
 while True:
-    c.execute(
-        "SELECT * FROM actions WHERE processed = 0 ORDER BY added ASC LIMIT 1;"
-    )
-    results = c.fetchone()
-    if results:
-        print "new record: ", results
-        # todo - update record to processing here
-        print "processing..."
-        time.sleep(10)
-        # todo - update record to processed here
-    else:
-        # revert to default here
-        print "nothing found...waiting"
-        time.sleep(10)
+    with conn:
+        c = conn.cursor()
+        c.execute(
+            "SELECT * FROM actions WHERE processed = 0 ORDER BY added ASC LIMIT 1;"
+        )
+        result = c.fetchone()
+        if result:
+            print "new record: ", result
+            c.execute("UPDATE actions SET processing = 1 WHERE id = ?", (result[0],))
+            conn.commit()
+            print "processing..."
+            time.sleep(10)
+            c.execute(
+                "UPDATE actions SET processing = 0, processed = 1 WHERE id = ?",
+                (result[0],)
+            )
+            conn.commit()
+        else:
+            # revert to default here
+            print "nothing found...waiting"
+            time.sleep(10)
