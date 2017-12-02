43f9bc51a58837ac1938bd3f476c451075d70dc4
erictempleton1 <erictempleton1@gmail.com>
2017-12-01 21:06:34 -0500

    adding simple example for testing

---
+++ simple_example.py
@@ -0,0 +1,36 @@
+import time
+from neopixel import *
+
+LED_COUNT = 72
+LED_PIN = 18
+LED_FREQ_HZ = 800000
+LED_DMA = 5
+LED_BRIGHTNESS = 255
+LED_INVERT = False
+LED_CHANNEL = 0
+
+def color(r, g, b):
+    return Color(g, r, b)
+
+def color_wipe(strip, color, wait_ms=50):
+	"""Wipe color across display a pixel at a time."""
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
+
+if __name__ == "__main__":
+    strip = Adafruit_NeoPixel(
+        LED_COUNT,
+        LED_PIN,
+        LED_FREQ_HZ,
+        LED_DMA,
+        LED_BRIGHTNESS,
+        LED_INVERT,
+        LED_CHANNEL
+    )
+    strip.begin()
+    while True:
+        print "color wipe"
+        color_wipe(strip, color(255, 255, 255)
